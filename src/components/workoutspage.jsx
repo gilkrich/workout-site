8a1f91cb1878275d576f3138e9ffe64d54326208
kim-gusovsky <kimgusovsky4@gmail.com>
2023-05-31 09:33:10 +0300

    addedworkoutcard

--- src/components/workoutspage.jsx
+++ src/components/workoutspage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Outlet, Link, useParams } from 'react-router-dom'
-
-const Workoutspage = () => {
+import WorkoutCard from './WorkoutCard'
+const Workoutspage = ({workoutData,setWorkoutData}) => {
   return (
-    <div>
-      workouts
+    <div style={{display:"flex", flexWrap:"wrap",gap:"30px",justifyContent:"center"}}>
+      {workoutData?.map((item,index)=>{
+        return(
+        <WorkoutCard key={index}
+         image={item.img}
+          name={item.name}
+          goal={item.goal} 
+          difficulty={item.level}
+          exercises={item.exercises}
+          equipment={item.equipment}/>
+        )
+      })}
     </div>
   )
 }
