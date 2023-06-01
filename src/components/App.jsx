dceb4eb01211b3e72e4ecc9368bab2e892c671e8
gilkrich <gildebil2@gmail.com>
2023-06-01 09:11:31 +0300

    workplanupdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,22 @@ import Workoutspage from './Workoutspage';
 import Homepage from './Homepage';
 import {data} from './workouts.json'
 import WorkoutCard from './WorkoutCard';
+import Workoutplanpage from './Workoutplanpage';
 function App() {
  const [workoutData,setWorkoutData] =useState();
  useEffect(()=>{
-   setWorkoutData(data?.manworkout);
- },[])
+   setWorkoutData(data&&data?.manworkout);
+   console.log("mounted")
+  },[])
+
   return (
     <>
     <div style={{maxWidth:"100vw"}}>
     <Routes>
     <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Homepage/>}></Route>
+          <Route path='/' element={<Homepage workoutData={workoutData}/>}></Route>
           <Route path='workouts' element={<Workoutspage workoutData={workoutData} setWorkoutData={setWorkoutData}/>}></Route>
+          <Route path='workouts/:name' element={< Workoutplanpage workoutData={workoutData} setWorkoutData={setWorkoutData}/>}></Route>
           </Route>
     </Routes>
     </div>
